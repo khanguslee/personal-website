7ab50fb242c9fb733e345bdf7f5466b73dc27c5e
khanguslee <khanguslee@gmail.com>
2020-02-13 00:35:40 +1100

    Refactor interests section into a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -9,6 +9,7 @@ import Layout from '../components/Layout';
 
 import Sidebar from '../components/Sidebar';
 import config from '../../config';
+import Interests from './interests';
 
 const IndexPage = () => (
   <Layout>
@@ -31,26 +32,7 @@ const IndexPage = () => (
 
       <hr className="m-0" />
 
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="interests"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Interests</h2>
-          <p>
-            Apart from being a web developer, I enjoy most of my time being
-            outdoors. In the winter, I am an avid skier and novice ice climber.
-            During the warmer months here in Colorado, I enjoy mountain biking,
-            free climbing, and kayaking.
-          </p>
-          <p className="mb-0">
-            When forced indoors, I follow a number of sci-fi and fantasy genre
-            movies and television shows, I am an aspiring chef, and I spend a
-            large amount of my free time exploring the latest technology
-            advancements in the front-end web development world.
-          </p>
-        </div>
-      </section>
+      <Interests />
 
       <hr className="m-0" />
 
