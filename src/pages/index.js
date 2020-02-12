b170142b3be45011adcd143a1898a393792c6dd5
khanguslee <khanguslee@gmail.com>
2020-02-13 00:22:42 +1100

    Refactor education section into a component

--- src/pages/index.js
+++ src/pages/index.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 import About from './about';
 import Experience from './experience';
+import Education from './education';
+
 import Layout from '../components/Layout';
 
 import Sidebar from '../components/Sidebar';
 import config from '../../config';
+
 const IndexPage = () => (
   <Layout>
     <Sidebar />
@@ -19,37 +22,7 @@ const IndexPage = () => (
 
       <hr className="m-0" />
 
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="education"
-      >
-        <div className="w-100">
-          <h2 className="mb-5">Education</h2>
-
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-            <div className="resume-content">
-              <h3 className="mb-0">University of Colorado Boulder</h3>
-              <div className="subheading mb-3">Bachelor of Science</div>
-              <div>Computer Science - Web Development Track</div>
-              <p>GPA: 3.23</p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">August 2006 - May 2010</span>
-            </div>
-          </div>
-
-          <div className="resume-item d-flex flex-column flex-md-row justify-content-between">
-            <div className="resume-content">
-              <h3 className="mb-0">James Buchanan High School</h3>
-              <div className="subheading mb-3">Technology Magnet Program</div>
-              <p>GPA: 3.56</p>
-            </div>
-            <div className="resume-date text-md-right">
-              <span className="text-primary">August 2002 - May 2006</span>
-            </div>
-          </div>
-        </div>
-      </section>
+      <Education />
 
       <hr className="m-0" />
 
