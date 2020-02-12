7a72173f11854ad9fde29c6a4f067e6c915c51c2
khanguslee <khanguslee@gmail.com>
2020-02-13 00:13:36 +1100

    Refactor about section into a component

---
+++ src/pages/about.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+function About(props) {
+    const { config } = props;
+
+    const renderSocialLink = (config) => {
+        return config.socialLinks.map(social => {
+            const { icon, url } = social;
+            return (
+                <a key={url} href={url}>
+                    <i className={`fab ${icon}`}></i>
+                </a>
+            );
+        })
+    }
+
+    return (
+        <section
+            className="resume-section p-3 p-lg-5 d-flex align-items-center"
+            id="about"
+        >
+            <div className="w-100">
+                <h1 className="mb-0">
+                    {config.firstName}
+                    <span className="text-primary">{config.lastName}</span>
+                </h1>
+                <div className="subheading mb-5">
+                    <a href={`mailto:${config.email}`}>{config.email}</a>
+                </div>
+                <p className="lead mb-5">
+                    I am a Software Engineer with a passion for solving real-world problems that impact people's lives through programming.
+                </p>
+                <div className="social-icons">
+                    {renderSocialLink(config)}
+                </div>
+            </div>
+        </section>
+    )
+}
+
+export default About;
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,44 +1,16 @@
 import React from 'react';
 
+import About from './about';
 import Layout from '../components/Layout';
 
-import { Link } from 'gatsby';
 import Sidebar from '../components/Sidebar';
 import config from '../../config';
 const IndexPage = () => (
   <Layout>
     <Sidebar />
     <div className="container-fluid p-0">
-      <section
-        className="resume-section p-3 p-lg-5 d-flex align-items-center"
-        id="about"
-      >
-        <div className="w-100">
-          <h1 className="mb-0">
-            {config.firstName}
-            <span className="text-primary">{config.lastName}</span>
-          </h1>
-          <div className="subheading mb-5">
-            <a href={`mailto:${config.email}`}>{config.email}</a>
-          </div>
-          <p className="lead mb-5">
-            I am experienced in leveraging agile frameworks to provide a robust
-            synopsis for high level overviews. Iterative approaches to corporate
-            strategy foster collaborative thinking to further the overall value
-            proposition.
-          </p>
-          <div className="social-icons">
-            {config.socialLinks.map(social => {
-              const { icon, url } = social;
-              return (
-                <a key={url} href={url}>
-                  <i className={`fab ${icon}`}></i>
-                </a>
-              );
-            })}
-          </div>
-        </div>
-      </section>
+
+      <About config={config} />
 
       <hr className="m-0" />
 
