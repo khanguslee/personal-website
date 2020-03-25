02008b533f570ac7731633bb86a5e4976dac84f8
Angus Lee <angus.lee809@gmail.com>
2020-03-25 18:55:09 +1100

    Refactor Experience component to get experiences from an array (#6)
    * Refactor config to have manifest values inside a dictionary
    
    * Refactor experience component to take in experiences from config
    
    * Add missing internships into Experiences
    
    * Add todos into Experience component
    
    * Update manifest names
    
    * Update Experience unit test to test experience prop
    
    * Fix misspelt variable in gatsby config

--- config.js
+++ config.js
@@ -1,12 +1,14 @@
 module.exports = {
   siteTitle: 'khanguslee', // <title>
-  manifestName: 'Resume',
-  manifestShortName: 'Landing', // max 12 characters
-  manifestStartUrl: '/',
-  manifestBackgroundColor: '#663399',
-  manifestThemeColor: '#663399',
-  manifestDisplay: 'standalone',
-  manifestIcon: 'src/assets/img/website-icon.png',
+  manifest: {
+    name: 'khanguslee Portfolio Website',
+    shortName: 'khanguslee', // Max 12 characters
+    startUrl: '/',
+    backgroundColor: '#663399',
+    themeColor: '#663399',
+    display: 'standalone',
+    icon: 'src/assets/img/website-icon.png',
+  },
   pathPrefix: `/gatsby-starter-resume/`, // This path is subpath of your hosting https://domain/portfolio
   firstName: 'Angus',
   lastName: 'Lee',
@@ -24,4 +26,38 @@ module.exports = {
     },
   ],
   email: 'khanguslee@gmail.com',
+  experience: [
+    {
+      company: 'MongoDB',
+      role: 'Software Engineer Intern',
+      startDate: 'December 2019',
+      endDate: 'February 2019',
+      description:
+        'Part of the MongoDB Charts Team working on incorporating interactive Charts into MongoDB Charts',
+    },
+    {
+      company: 'Australian Bureau of Statistics',
+      role: 'Software Engineer Intern',
+      startDate: 'June 2019',
+      endDate: 'December 2019',
+      description:
+        'Developed a progressive web application for Census Field Officers using typescript and react. Liasing with business clients to fulfil their needs about the product',
+    },
+    {
+      company: 'MongoDB',
+      role: 'Software Engineer Intern',
+      startDate: 'December 2018',
+      endDate: 'March 2019',
+      description:
+        'Part of the Technical Services MongoDB Team. Worked on a POC intern project that allows collaborative log viewing between Technical Support staff',
+    },
+    {
+      company: 'Autodesk',
+      role: 'Software Engineer Intern',
+      startDate: 'July 2017',
+      endDate: 'January 2018',
+      description:
+        'Converted a Windows-only C# application into a program that builds and runs on Linux using Mono C#',
+    },
+  ],
 };
--- src/components/__tests__/experience.js
+++ src/components/__tests__/experience.js
@@ -7,10 +7,41 @@ describe('Experience component', () => {
   const experienceTestID = 'experience-section';
 
   it('displays correct header', () => {
-    const { getByTestId } = render(<Experience />);
+    const testExperiences = [];
+    const { getByTestId } = render(
+      <Experience experiences={testExperiences} />
+    );
 
     const expectedHeaderText = 'Experience';
 
     expect(getByTestId(experienceTestID)).toHaveTextContent(expectedHeaderText);
+  });
+
+  it('displays a single experience correctly', () => {
+    const testCompanyExperience = {
+      company: 'Test Company',
+      role: 'Test Role',
+      startDate: 'January 2020',
+      endDate: 'December 2020',
+      description: 'Hello World',
+    };
+    const testExperiences = [testCompanyExperience];
+
+    const { getByText, getByTestId } = render(
+      <Experience experiences={testExperiences} />
+    );
+
+    const experienceContent = getByTestId('experience-content');
+    expect(experienceContent).toHaveTextContent(testCompanyExperience.role);
+    expect(experienceContent).toHaveTextContent(testCompanyExperience.company);
+    expect(experienceContent).toHaveTextContent(
+      testCompanyExperience.description
+    );
+
+    expect(getByText(testCompanyExperience.company)).toHaveClass('subheading');
+
+    const experienceDate = getByTestId('experience-date');
+    const expectedDateText = `${testCompanyExperience.startDate} - ${testCompanyExperience.endDate}`;
+    expect(experienceDate).toHaveTextContent(expectedDateText);
   });
 });
