cd292a423fe690a956b7fca1366b53d2a29060fa
khanguslee <khanguslee@gmail.com>
2020-03-25 17:01:10 +1100

    Reformat Education component with prettier

--- src/components/education.js
+++ src/components/education.js
@@ -1,28 +1,31 @@
 import React from 'react';
 
 function Education() {
-    return (
-        <section
-            className="resume-section p-3 p-lg-5 d-flex align-items-center"
-            id="education"
-        >
-            <div className="w-100">
-                <h2 className="mb-5">Education</h2>
+  return (
+    <section
+      className="resume-section p-3 p-lg-5 d-flex align-items-center"
+      id="education"
+      data-testid="education-section"
+    >
+      <div className="w-100">
+        <h2 className="mb-5">Education</h2>
 
-                <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-                    <div className="resume-content">
-                        <h3 className="mb-0">Monash University</h3>
-                        <div className="subheading mb-3">Bachelor of Science / Bachelor of Engineering (Honours)</div>
-                        <div>Computational Science (Extended major)</div>
-                        <div>Electrical and Computer Systems Engineering</div>
-                    </div>
-                    <div className="resume-date text-md-right">
-                        <span className="text-primary">February 2015 - June 2020</span>
-                    </div>
-                </div>
+        <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
+          <div className="resume-content">
+            <h3 className="mb-0">Monash University</h3>
+            <div className="subheading mb-3">
+              Bachelor of Science / Bachelor of Engineering (Honours)
             </div>
-        </section>
-    )
+            <div>Computational Science (Extended major)</div>
+            <div>Electrical and Computer Systems Engineering</div>
+          </div>
+          <div className="resume-date text-md-right">
+            <span className="text-primary">February 2015 - June 2020</span>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Education;
