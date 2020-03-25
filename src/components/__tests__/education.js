d9cb5c84b4a1079cf328bb925948d9cec0e3c86b
khanguslee <khanguslee@gmail.com>
2020-03-25 17:01:33 +1100

    Test Education component renders section header properly

---
+++ src/components/__tests__/education.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { render } from '@testing-library/react';
+
+import Education from '../education';
+
+describe('Education component', () => {
+  const educationTestID = 'education-section';
+
+  it('displays correct header', () => {
+    const { getByTestId } = render(<Education />);
+
+    const expectedHeaderText = 'Education';
+
+    expect(getByTestId(educationTestID)).toHaveTextContent(expectedHeaderText);
+  });
+});
