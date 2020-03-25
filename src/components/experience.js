473b22bc9928238531d6124a9ecaed65272b3ee9
khanguslee <khanguslee@gmail.com>
2020-03-25 18:47:06 +1100

    Update Experience unit test to test experience prop

--- src/components/experience.js
+++ src/components/experience.js
@@ -5,17 +5,23 @@ function Experience(props) {
 
   const renderJobExperiences = experiences => {
     // TODO: Sort the experiences array before rendering.
-    return experiences.map(job => {
+    return experiences.map((job, index) => {
       const { company, role, startDate, endDate, description } = job;
       // TODO: Add image of company beside each entry
       return (
-        <div className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5">
-          <div className="resume-content">
+        <div
+          className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
+          key={index}
+        >
+          <div className="resume-content" data-testid="experience-content">
             <h3 className="mb-0">{role}</h3>
             <div className="subheading mb-3">{company}</div>
             <p>{description}</p>
           </div>
-          <div className="resume-date text-md-right">
+          <div
+            className="resume-date text-md-right"
+            data-testid="experience-date"
+          >
             <span className="text-primary">
               {startDate} - {endDate}
             </span>
