3f579bd6bf07a1056a0f9a5ca283c4b795b392eb
khanguslee <khanguslee@gmail.com>
2020-03-26 22:52:21 +1100

    Update SectionItem component with descriptionList prop

--- src/components/SectionItem.js
+++ src/components/SectionItem.js
@@ -1,18 +1,41 @@
 import React from 'react';
 
 function SectionItem(props) {
-  const { key, title, subtitle, description, rightTitle } = props;
+  const {
+    id,
+    title,
+    subtitle,
+    description,
+    descriptionList,
+    rightTitle,
+    testId,
+  } = props;
 
   // TODO: Add image of company beside each entry
+
+  const renderDescriptionList = descriptionList => {
+    const descriptionListItems = descriptionList.map(
+      (descriptionListItem, index) => {
+        return (
+          <li key={`${descriptionListItem}-${index}`}>{descriptionListItem}</li>
+        );
+      }
+    );
+
+    return <ul>{descriptionListItems}</ul>;
+  };
+
   return (
     <div
       className="resume-item d-flex flex-column flex-md-row justify-content-between mb-5"
-      key={key}
+      data-testid={testId}
+      key={id}
     >
       <div className="resume-content" data-testid="section-content">
         <h3 className="mb-0">{title}</h3>
-        <div className="subheading mb-3">{subtitle}</div>
-        <p>{description}</p>
+        <div className="subheading mb-1">{subtitle}</div>
+        {description && <p>{description}</p>}
+        {descriptionList && renderDescriptionList(descriptionList)}
       </div>
       <div
         className="resume-date text-md-right"
