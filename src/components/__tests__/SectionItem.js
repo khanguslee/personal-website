10b9cb4ceb02a5d95d660c54e49b1df48bb905f3
khanguslee <khanguslee@gmail.com>
2020-03-26 23:31:51 +1100

    Add unit test for SectionItem description

--- src/components/__tests__/SectionItem.js
+++ src/components/__tests__/SectionItem.js
@@ -30,4 +30,27 @@ describe('SectionItem component', () => {
       testSectionConfig.rightTitle
     );
   });
+
+  it('displays description', () => {
+    const testSectionConfig = {
+      title: 'Test Title',
+      subtitle: 'Test Subtitle',
+      rightTitle: 'Test Right Title',
+      description: 'Test description',
+    };
+    const sectionTestId = 'test-section-id';
+    const { getByTestId } = render(
+      <SectionItem
+        id={0}
+        title={testSectionConfig.title}
+        subtitle={testSectionConfig.subtitle}
+        description={testSectionConfig.description}
+        rightTitle={testSectionConfig.rightTitle}
+        testId={sectionTestId}
+      />
+    );
+    expect(getByTestId(sectionTestId)).toHaveTextContent(
+      testSectionConfig.description
+    );
+  });
 });
